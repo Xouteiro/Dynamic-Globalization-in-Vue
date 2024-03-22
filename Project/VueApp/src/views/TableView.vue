e4951930567a24ac48e1a62fa835b060a00d31f8
xouteiro <xavierouteiro@gmail.com>
2024-03-22 14:40:18 +0000

    table working

--- Project/VueApp/src/views/TableView.vue
+++ Project/VueApp/src/views/TableView.vue
@@ -3,27 +3,29 @@
 import { idioms } from '@/i18n.js';
 import i18n from '@/i18n.js';
 import  utils  from '@/utils.js';
-import { on } from 'events';
-import { onMounted } from 'vue';
 import { reactive } from 'vue';
 import { useI18n } from "vue-i18n";
 
 
 let { locale } = useI18n()
 let currentMessages = i18n.global.getLocaleMessage(locale.value);
-let currentInput: any = [];
+let currentInput: any = reactive([]);
+populateCurrentInput();
 
-// function populateCurrentInput() {
-//   for (let i = 0; i < idioms.length; i++) {
-//     let vocabulary = getVocabulary(idioms[i].name);
-//     for (let key in vocabulary) {
-//       currentInput[key] = vocabulary[key];
-//     }
-//   }
-// }
+function populateCurrentInput() {
+  for (let i = 0; i < idioms.length; i++) {
+    let vocabulary = getVocabulary(idioms[i].name);
+    for (let key in vocabulary) {
+      let finalKey = idioms[i].name + "." + key;
+      if (!currentInput[finalKey]) {
+        currentInput[finalKey] = vocabulary[key];
+      }
+    }
+  }
+}
 
 function debug(key: any) {
-  console.log(key);
+  console.log(currentInput['pt.About']);
 }
 
 function getVocabulary(idiom: string) {
@@ -35,9 +37,7 @@ function getVocabulary(idiom: string) {
 }
 
 
-// onMounted(() => {
-//   populateCurrentInput();
-// });
+
  
 </script>
 
@@ -50,18 +50,22 @@ function getVocabulary(idiom: string) {
       <th class="idiom"> Idiom </th>
       <th class="identifier">Identifier</th>
       <th class="text"> Text </th>
+      <th class="submit">Submit</th>
     </tr>
   </thead>
   <tbody v-for="(item, index) in idioms" :key="index">
     <tr v-for="(word, key) in getVocabulary(item.name)" :key="key">
       <td class="idiom"> {{ item.name }} </td>
       <td class="identifier"> {{ key }}</td> 
-      <td class="text"><input type="text" :placeholder="currentInput[key]" v-model="currentInput[key]" /> </td>
-      <td class="submit"><button @click="utils.updateElement(key.toString(), currentInput[key], item.name, currentMessages)" >Submit</button></td>
+      <td class="text"><input type="text" v-model="currentInput[item.name + '.' + key]" /></td>
+      <td class="submit"><button @click="utils.updateElement(key.toString(), currentInput[item.name + '.' + key], item.name, currentMessages)" >Submit</button></td>
     </tr>
   </tbody>
 </table>
+
+  <button @click="debug">Debug</button>
 </template>
+
 
 
 <style scoped>
@@ -85,7 +89,7 @@ h1 {
 }
 
 
-.idiom, .text, .identifier, .idiom-header, .text-header, .identifier-header{
+.idiom, .text, .identifier, .submit{
   border: 1px solid;
   padding: 8px;
   text-align: center;
